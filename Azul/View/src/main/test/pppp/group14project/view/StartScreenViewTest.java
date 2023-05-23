f0da638a81a8e76cef36f64163a1ac720b9116ca
frankavanjaarsveld <franka.vanjaarsveld@gmail.com>
2023-05-23 17:04:30 +0200

    made a test to check if a user can enter their name and start the game

---
+++ Azul/View/src/main/test/pppp/group14project/view/StartScreenViewTest.java
@@ -0,0 +1,49 @@
+package pppp.group14project.view;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
+import javafx.stage.Stage;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.testfx.api.FxToolkit;
+import org.testfx.framework.junit.ApplicationTest;
+import org.testfx.util.WaitForAsyncUtils;
+
+import static org.testfx.api.FxAssert.*;
+import static org.testfx.matcher.control.LabeledMatchers.hasText;
+
+public class StartScreenViewTest extends ApplicationTest {
+
+    @Override
+    public void start (Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("start-screen-view.fxml"));
+        stage.setScene(new Scene(root,320, 240));
+//        stage.setTitle("Azul is the best game ever!");
+        stage.show();
+        stage.toFront();
+    }
+
+    @Before
+    public void setUp () throws Exception {
+    }
+
+    @After
+    public void tearDown () throws Exception {
+        FxToolkit.hideStage();
+        release(new KeyCode[]{});
+        release(new MouseButton[]{});
+    }
+
+    @Test
+    public void firstTest () {
+        clickOn("#usernameTextField");
+        write("Bob");
+        clickOn(".button");
+        WaitForAsyncUtils.waitForFxEvents();
+        verifyThat("#welcomeText", hasText("Welcome Bob!"));
+    }
+}
