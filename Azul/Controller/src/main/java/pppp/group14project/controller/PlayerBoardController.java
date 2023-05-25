de11c7b492b6526f381d3e5c7882c4bff26b45fc
Fabian <fabi.staber@gmx.at>
2023-05-25 18:25:34 +0200

    [VIEW] integrate floor tile view into player board view

--- Azul/Controller/src/main/java/pppp/group14project/controller/PlayerBoardController.java
+++ Azul/Controller/src/main/java/pppp/group14project/controller/PlayerBoardController.java
@@ -1,13 +1,37 @@
 package pppp.group14project.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 
-public class PlayerBoardController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PlayerBoardController implements Initializable {
   @FXML
   public Label playerName;
 
+  @FXML
+  private GridPane playerBoardGrid;
+
   public void setPlayerName(String playerName) {
     this.playerName.setText(playerName);
+  }
+
+  @Override
+  public void initialize(URL url, ResourceBundle resourceBundle) {
+    try {
+      FXMLLoader loader = new FXMLLoader(getClass().getResource("/player-floor-view.fxml"));
+      GridPane playerFloor = loader.load();
+//      FloorController floorController = loader.getController();
+      playerBoardGrid.add(playerFloor, 0, 2);
+
+    } catch (
+        IOException e) {
+      e.printStackTrace();
+    }
   }
 }
