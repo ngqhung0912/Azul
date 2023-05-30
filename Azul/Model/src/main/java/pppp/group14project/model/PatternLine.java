020d3b9d4e91b950d2a1d7cc0636d1ad9d05453d
Lenn <lennperik@hotmail.nl>
2023-05-30 15:45:53 +0200

    Add PatternLine model stub

---
+++ Azul/Model/src/main/java/pppp/group14project/model/PatternLine.java
@@ -0,0 +1,55 @@
+package pppp.group14project.model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class PatternLine {
+
+    private List<Tile> tiles;
+
+    public PatternLine(int numberOfTiles) {
+        // Constant number of elements
+        this.tiles = new ArrayList<>(Collections.nCopies(numberOfTiles, null));
+    }
+
+    /**
+     * Adds tiles to the PatternLine, returns the excess tiles to add to the floor
+     * @param t the tiles to add
+     * @return the excess tiles
+     */
+    public List<Tile> addTiles(List<Tile> t) {
+        return null;
+    }
+
+    /**
+     * Empties the PatternLine, and returns a Tile to be placed on the Wall
+     * @return the Tile to be moved to the Wall
+     */
+    public Tile moveTiles() {
+        return null;
+    }
+
+    /**
+     * Returns the color of the PatternLine, null if no tiles are in the PatternLine
+     * @return
+     */
+    public Tile getColor() {
+        return null;
+    }
+
+    /**
+     * Empties the PatternLine
+     */
+    public void empty() {
+    }
+
+    /**
+     * Used to see if the PatternLine is full
+     * @return if the PatternLine is full
+     */
+    public boolean isFull() {
+        return false;
+    }
+
+}
