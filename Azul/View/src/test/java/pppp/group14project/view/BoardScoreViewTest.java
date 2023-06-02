1f9bc11579f0f3fbcec35037aa391c4edca71c12
Fabian <fabi.staber@gmx.at>
2023-06-02 16:04:27 +0200

    [TEST] add test to check correct score display at the start of a game

---
+++ Azul/View/src/test/java/pppp/group14project/view/BoardScoreViewTest.java
@@ -0,0 +1,42 @@
+package pppp.group14project.view;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.testfx.api.FxToolkit;
+import org.testfx.framework.junit5.ApplicationTest;
+
+import static org.testfx.api.FxAssert.verifyThat;
+import static org.testfx.util.NodeQueryUtils.hasText;
+
+public class BoardScoreViewTest extends ApplicationTest {
+
+  @BeforeAll
+  public static void headless() {
+    if (Boolean.parseBoolean(System.getProperty("gitlab-ci", "false"))) {
+      GitlabCISupport.headless();
+    }
+  }
+
+  @Override
+  public void start(Stage stage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("/player-board-view.fxml"));
+    stage.setScene(new Scene(root, 500, 500));
+    stage.show();
+    stage.toFront();
+  }
+
+  @AfterEach
+  public void tearDown() throws Exception {
+    FxToolkit.hideStage();
+  }
+
+  @Test
+  public void whenGameStartsThenScoreIsZero() {
+    verifyThat("#scoreText", hasText("0"));
+  }
+}
