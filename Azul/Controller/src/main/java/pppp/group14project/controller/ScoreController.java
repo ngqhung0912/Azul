d84a0c1168f9a658d9906e851cd54ae63f70d640
Fabian <fabi.staber@gmx.at>
2023-06-05 10:30:28 +0200

    [ADD] add update score method to ScoreController

--- Azul/Controller/src/main/java/pppp/group14project/controller/ScoreController.java
+++ Azul/Controller/src/main/java/pppp/group14project/controller/ScoreController.java
@@ -3,6 +3,7 @@ package pppp.group14project.controller;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.text.Text;
+import pppp.group14project.model.Board;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -20,5 +21,17 @@ public class ScoreController implements Initializable {
   @Override
   public void initialize(URL url, ResourceBundle resourceBundle) {
     setScoreText("0");
+  }
+
+  public void updateScore(Board board) {
+    // additional check if currently displayed score is the same as the score in the model
+    int displayedScore = Integer.parseInt(scoreText.getText());
+    int modelScore = board.getScore();
+    assert displayedScore == modelScore;
+
+    int floorScore = board.getFloor().getScore();
+    int wallScore = board.getWall().getWallScore();
+    board.setScore(modelScore + floorScore + wallScore);
+    setScoreText(Integer.toString(board.getScore()));
   }
 }
