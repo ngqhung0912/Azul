81cb0e60549964d7e0bededbc41b8eed2d09072b
frankavanjaarsveld <franka.vanjaarsveld@gmail.com>
2023-06-08 15:07:58 +0200

    [TEST] tiles change colour on hover to show they are clickable

--- Azul/View/src/test/java/pppp/group14project/view/FactoriesViewTest.java
+++ Azul/View/src/test/java/pppp/group14project/view/FactoriesViewTest.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Paint;
 import javafx.scene.layout.GridPane;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
@@ -12,6 +13,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.testfx.api.FxToolkit;
 import org.testfx.framework.junit5.ApplicationTest;
+import org.testfx.util.WaitForAsyncUtils;
 import pppp.group14project.controller.ClickableTile;
 import pppp.group14project.controller.FactoryController;
 
@@ -43,15 +45,31 @@ public class FactoriesViewTest extends ApplicationTest {
 
     @Test
     public void tilesAreClickable() {
-        GridPane factory = lookup("#tileGrid").query();
+        GridPane tileGrid = lookup("#tileGrid").query();
         int expected = 4;
         int actual = 0;
-        for(Node n: factory.getChildren()) {
+        for(Node n: tileGrid.getChildren()) {
             if(n instanceof ClickableTile) {
                 actual++;
             }
         }
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void tilesLookClickable() {
+        GridPane tileGrid = lookup("#tileGrid").query();
+        for(Node n : tileGrid.getChildren()) {
+            ClickableTile tile = (ClickableTile) n;
+            Paint bordercolour = tile.getBorder().getStrokes().get(0).getLeftStroke();
+            Paint backgroundcolor = tile.getBackground().getFills().get(0).getFill();
+            assertNotEquals(bordercolour, backgroundcolor);
+            moveTo(tile);
+            WaitForAsyncUtils.waitForFxEvents();
+            backgroundcolor = tile.getBackground().getFills().get(0).getFill();
+            assertEquals(bordercolour, backgroundcolor);
+        }
+
     }
 
 }
