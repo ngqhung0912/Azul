cad7aead7a532169dc949ddc03cd39092949694b
frankavanjaarsveld <franka.vanjaarsveld@gmail.com>
2023-06-08 14:03:36 +0200

    [TEST] The creation of the correct amount of boards and factories for 2,3 and 4 number of players

--- Azul/View/src/test/java/pppp/group14project/view/StartScreenViewTest.java
+++ Azul/View/src/test/java/pppp/group14project/view/StartScreenViewTest.java
@@ -1,19 +1,26 @@
 package pppp.group14project.view;
 
+import javafx.beans.Observable;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseButton;
+import javafx.scene.layout.GridPane;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.*;
 import org.testfx.api.FxToolkit;
 import org.testfx.util.NodeQueryUtils;
 import org.testfx.util.WaitForAsyncUtils;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.testfx.api.FxAssert.*;
 import static org.testfx.matcher.control.LabeledMatchers.hasText;
 import org.testfx.framework.junit5.ApplicationTest;
+import pppp.group14project.model.Game;
 
 public class StartScreenViewTest extends ApplicationTest {
     @BeforeAll
@@ -26,7 +33,7 @@ public class StartScreenViewTest extends ApplicationTest {
     @Override
     public void start (Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/start-screen-view.fxml"));
-        stage.setScene(new Scene(root,320, 240));
+        stage.setScene(new Scene(root,1250, 700));
         stage.show();
         stage.toFront();
     }
@@ -40,6 +47,8 @@ public class StartScreenViewTest extends ApplicationTest {
         FxToolkit.hideStage();
         release(new KeyCode[]{});
         release(new MouseButton[]{});
+        Game game = Game.getInstance();
+        game.removeAllBoards();
     }
 
     @Test
@@ -52,6 +61,69 @@ public class StartScreenViewTest extends ApplicationTest {
         clickOn("#startButton");
         WaitForAsyncUtils.waitForFxEvents();
         verifyThat("#playerName", NodeQueryUtils.hasText("Bob"));
+    }
 
+    @Test
+    public void correctNumberOfElementsTwoPlayers() {
+        clickOn("#usernameTextField");
+        write("Bob");
+        clickOn("#addPlayerButton");
+        clickOn("#addPlayerButton");
+        WaitForAsyncUtils.waitForFxEvents();
+        clickOn("#startButton");
+        WaitForAsyncUtils.waitForFxEvents();
+        GridPane innerGridLeft = lookup("#innerGridLeft").query();
+        GridPane innerGridRight = lookup("#innerGridRight").query();
+        assertEquals(1, countObjectInGrid("playerBoardGrid", innerGridLeft));
+        assertEquals(1, countObjectInGrid("playerBoardGrid", innerGridRight));
+        GridPane factoriesGrid = lookup("#factoriesGrid").query();
+        assertEquals(5, countObjectInGrid("factoryGrid", factoriesGrid));
+    }
+
+    @Test
+    public void correctNumberOfElementsThreePlayers() {
+        clickOn("#usernameTextField");
+        write("Bob");
+        clickOn("#addPlayerButton");
+        clickOn("#addPlayerButton");
+        clickOn("#addPlayerButton");
+        WaitForAsyncUtils.waitForFxEvents();
+        clickOn("#startButton");
+        WaitForAsyncUtils.waitForFxEvents();
+        GridPane innerGridLeft = lookup("#innerGridLeft").query();
+        GridPane innerGridRight = lookup("#innerGridRight").query();
+        assertEquals(2, countObjectInGrid("playerBoardGrid", innerGridLeft));
+        assertEquals(1, countObjectInGrid("playerBoardGrid", innerGridRight));
+        GridPane factoriesGrid = lookup("#factoriesGrid").query();
+        assertEquals(7, countObjectInGrid("factoryGrid", factoriesGrid));
+    }
+
+    @Test
+    public void correctNumberOfElementsFourPlayers() {
+        clickOn("#usernameTextField");
+        write("Bob");
+        clickOn("#addPlayerButton");
+        clickOn("#addPlayerButton");
+        clickOn("#addPlayerButton");
+        clickOn("#addPlayerButton");
+        WaitForAsyncUtils.waitForFxEvents();
+        clickOn("#startButton");
+        WaitForAsyncUtils.waitForFxEvents();
+        GridPane innerGridLeft = lookup("#innerGridLeft").query();
+        GridPane innerGridRight = lookup("#innerGridRight").query();
+        assertEquals(2, countObjectInGrid("playerBoardGrid", innerGridLeft));
+        assertEquals(2, countObjectInGrid("playerBoardGrid", innerGridRight));
+        GridPane factoriesGrid = lookup("#factoriesGrid").query();
+        assertEquals(9, countObjectInGrid("factoryGrid", factoriesGrid));
+    }
+
+    private int countObjectInGrid(String id, GridPane grid) {
+        int count = 0;
+        for(Node n: grid.getChildren()) {
+            if(n.getId().equals(id)) {
+                count++;
+            }
+        }
+        return count;
     }
 }
