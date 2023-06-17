142e85c355b23abf06790cf3ded43315356aa538
ngqhung0912 <67213674+ngqhung0912@users.noreply.github.com>
2023-06-17 16:04:38 +0200

    [REFACTOR] all test case use addTile with Tile and Row as signature instead of addTile(Tile, Row, Column).

--- Azul/Model/src/test/java/pppp/group14project/model/WallTest.java
+++ Azul/Model/src/test/java/pppp/group14project/model/WallTest.java
@@ -2,6 +2,7 @@ package pppp.group14project.model;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import pppp.group14project.model.exceptions.FullException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,61 +17,93 @@ class WallTest {
 
     @Test
     void addSpecificTile() {
-        wall.addTile(Tile.RED, 3, 0);
+        try {
+            wall.addTile(Tile.RED, 3);
+        } catch (FullException e) {
+            fail();
+        }
         assertEquals(1, wall.countNonNullElementsInRow(wall.getRow(3)));
-        assertEquals(Tile.RED, wall.getRow(3).get(0));
+        assertTrue(wall.isTileInRow(Tile.RED, 3));
 
     }
 
     @Test
     void incorrectTileLocation() {
         assertThrows(IndexOutOfBoundsException.class, () -> {
-            wall.addTile(Tile.RED, 5, 2);
+            try {
+                wall.addTile(Tile.RED, 5);
+            } catch (FullException e) {
+                fail();
+            }
         });
     }
 
-    @Test
-    void tileIsAlreadyInRow() {
-        wall.addTile(Tile.RED, 3, 3);
-        assertTrue(wall.isTileInRow(Tile.RED, 3));
-    }
 
     @Test
     void rowIsFullCorrectColors() {
-        wall.addTile(Tile.BLACK, 0, 0);
-        wall.addTile(Tile.BLUE, 0, 1);
-        wall.addTile(Tile.WHITE, 0, 2);
-        wall.addTile(Tile.RED, 0, 3);
-        wall.addTile(Tile.ORANGE, 0, 4);
+        try {
+            wall.addTile(Tile.BLACK, 0);
+            wall.addTile(Tile.BLUE, 0);
+            wall.addTile(Tile.WHITE, 0);
+            wall.addTile(Tile.RED, 0);
+            wall.addTile(Tile.ORANGE, 0);
+        } catch (FullException e) {
+            fail();
+        }
+
         assertTrue(wall.isRowFull(wall.getRow(0)));
         assertEquals(5, wall.getRow(0).size());
     }
 
     @Test
-    void rowIsFullInorrectColors() {
-        wall.addTile(Tile.BLACK, 0, 0);
-        wall.addTile(Tile.BLACK, 0, 1);
-        wall.addTile(Tile.WHITE, 0, 2);
-        wall.addTile(Tile.RED, 0, 3);
-        wall.addTile(Tile.RED, 0, 4);
+    void AddSameTilesToOneRow() {
+
+        try {
+            wall.addTile(Tile.BLACK, 0);
+            wall.addTile(Tile.WHITE, 0);
+            wall.addTile(Tile.RED, 0);
+        } catch (FullException e) {
+            fail();
+        }
+        try {
+            wall.addTile(Tile.BLACK, 0);
+            fail();
+
+        } catch (FullException ignored) {}
+
+        try {
+            wall.addTile(Tile.RED, 0);
+            fail();
+
+        } catch (FullException ignored) {}
+
         assertNotEquals(5, wall.countNonNullElementsInRow(wall.getRow(0)));
     }
 
     @Test
     void rowIsNotFull() {
-        wall.addTile(Tile.RED, 0, 0);
-        wall.addTile(Tile.RED, 0, 1);
-        wall.addTile(Tile.RED, 0, 2);
-        wall.addTile(Tile.RED, 0, 3);
+        try {
+            wall.addTile(Tile.BLACK, 0);
+            wall.addTile(Tile.BLUE, 0);
+            wall.addTile(Tile.WHITE, 0);
+            wall.addTile(Tile.RED, 0);
+        } catch (FullException e) {
+            fail();
+        }
         assertFalse(wall.isRowFull(wall.getRow(0)));
     }
 
     @Test
     void emptyWall() {
-        wall.addTile(Tile.RED, 0, 0);
-        wall.addTile(Tile.RED, 3, 1);
-        wall.addTile(Tile.RED, 4, 2);
-        assertEquals(3, wall.getTilesInWall().size());
+        try {
+            wall.addTile(Tile.BLACK, 0);
+            wall.addTile(Tile.BLUE, 0);
+            wall.addTile(Tile.WHITE, 0);
+            wall.addTile(Tile.RED, 0);
+        } catch (FullException e) {
+            fail();
+        }
+        assertEquals(4, wall.getTilesInWall().size());
         wall.emptyWall();
         assertEquals(0, wall.getTilesInWall().size());
     }
@@ -78,37 +111,59 @@ class WallTest {
 
     @Test
     void scoreOfOneTile() {
-        wall.addTile(Tile.RED, 3, 2);
+        try {
+            wall.addTile(Tile.BLACK, 0);
+        } catch (FullException e) {
+            fail();
+        }
         assertEquals(1, wall.getWallScore());
     }
 
     @Test
     void scoreTwoNotConnectedTiles() {
-        wall.addTile(Tile.RED, 3, 2);
-        wall.addTile(Tile.BLUE, 4, 3);
+        try {
+            wall.addTile(Tile.BLACK, 0);
+            wall.addTile(Tile.BLUE, 0);
+        } catch (FullException e) {
+            fail();
+        }
+
         assertEquals(2, wall.getWallScore());
     }
 
     @Test
     void scoreTwoConnectedTiles() {
-        wall.addTile(Tile.RED, 3, 2);
-        wall.addTile(Tile.BLUE, 4, 2);
+        try {
+            wall.addTile(Tile.ORANGE, 3);
+            wall.addTile(Tile.BLUE, 4);
+        } catch (FullException e) {
+            fail();
+        }
         assertEquals(3, wall.getWallScore());
     }
 
     @Test
     void getFullColumnsandRows() {
-        wall.addTile(Tile.BLACK, 0, 1);
-        wall.addTile(Tile.BLUE, 1, 1);
-        wall.addTile(Tile.WHITE, 2, 1);
-        wall.addTile(Tile.RED, 3, 1);
-        wall.addTile(Tile.ORANGE, 4, 1);
-        assertEquals(1, wall.getFullCols());
-        wall.addTile(Tile.BLUE, 0, 0);
-        wall.addTile(Tile.WHITE, 0, 2);
-        wall.addTile(Tile.RED, 0, 3);
-        wall.addTile(Tile.ORANGE, 0, 4);
+        try {
+            wall.addTile(Tile.BLACK, 0);
+            wall.addTile(Tile.BLUE, 0);
+            wall.addTile(Tile.WHITE, 0);
+            wall.addTile(Tile.RED, 0);
+            wall.addTile(Tile.ORANGE, 0);
+        } catch (FullException e) {
+            fail();
+        }
         assertEquals(1, wall.getFullRows());
+
+        try {
+            wall.addTile(Tile.RED, 1);
+            wall.addTile(Tile.ORANGE, 2);
+            wall.addTile(Tile.BLUE, 3);
+            wall.addTile(Tile.WHITE, 4);
+        } catch (FullException e) {
+            fail();
+        }
+        assertEquals(1, wall.getFullCols());
     }
 
 
